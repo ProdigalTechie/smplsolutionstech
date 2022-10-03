44cc7fb1796f040aa221bb973bcd8072eb5600ba
kdbuss2 <kylebuss21@hotmail.com>
2022-10-03 06:27:47 -0500

    Email and PayPal contributor issues and Code Gen UI cleanup

--- SmplSolutionsTech/Models/App/ModelGeneratorModel.cs
+++ SmplSolutionsTech/Models/App/ModelGeneratorModel.cs
@@ -4,16 +4,15 @@ namespace SmplSolutionsTech.Models.App
 {
     public class ModelGeneratorModel
     {
-        [DisplayName("C# Data Type")]
+        [DisplayName("Data Type")]
         public int CSharpDataTypeId { get; set; } //=> EnumHelper.GetSelectList(typeof(CSharpDataTypesEnum));// DataTypesEnum dropdown
-        [DisplayName("SQL Data Type")]
+        [DisplayName("Data Type")]
         public int SqlDataTypeId { get; set; } //=> EnumHelper.GetSelectList(typeof(SqlDataTypesEnum));
         [DisplayName("Domain Access")]
         public int DomainAccess { get; set; } //=> EnumHelper.GetSelectList(typeof(AccessModifierEnum)); // AccessModifierEnum dropdown
-        [DisplayName("Property Name")]
         public string Name { get; set; }
-        [DisplayName("Custom Property Name")]
-        public string DataTypeName { get; set; } // used for custom datatypes
+        //[DisplayName("Custom Data Type")]
+        //public string DataTypeName { get; set; } // used for custom datatypes
         [DisplayName("Nullable")]
         public bool IsNullable { get; set; }
     }
